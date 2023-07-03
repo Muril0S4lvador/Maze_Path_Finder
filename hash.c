8bd565d2e7d8281852c630fc77f7060e64fab024
Murilo Salvador <109419353+Muril0S4lvador@users.noreply.github.com>
2023-07-03 18:45:58 -0300

    Iniciando implementar hash

---
+++ hash.c
@@ -0,0 +1,142 @@
+
+#include <stdlib.h>
+#include "hash.h"
+#include "forward_list.h"
+
+struct HashTable
+{
+    ForwardList **buckets;
+    HashFunction hash_fn;
+    CmpFunction cmp_fn;
+    int table_size;
+    int n_elements;
+};
+
+HashTableItem *_hash_pair_construct(void *key, void *val)
+{
+    HashTableItem *p = calloc(1, sizeof(HashTableItem));
+    p->key = key;
+    p->val = val;
+    return p;
+}
+
+void _hash_pair_destroy(HashTableItem *p)
+{
+    free(p);
+}
+
+HashTable *hash_table_construct(int table_size, HashFunction hash_fn, CmpFunction cmp_fn)
+{
+    HashTable *hash_tbl = calloc(1, sizeof(HashTable));
+
+    hash_tbl->table_size = table_size;
+    hash_tbl->hash_fn = hash_fn;
+    hash_tbl->cmp_fn = cmp_fn;
+    hash_tbl->buckets = calloc(table_size, sizeof(ForwardList *));
+    hash_tbl->n_elements = 0;
+
+    return hash_tbl;
+}
+
+void *hash_table_set(HashTable *h, void *key, void *val)
+{
+    int idx = h->hash_fn(h, key);
+    HashTableItem *item = _hash_pair_construct(key, val);
+
+    if(h->buckets[idx]){
+        Node *n = h->buckets[idx]->head;
+
+        while(n){
+            HashTableItem *prev = (HashTableItem*) n->value;
+            if(!h->cmp_fn(prev->key, key)){
+                void *v = n->value;
+                n->value = item;
+                return v;
+            }
+            n = n->next;
+        }
+
+    } else{
+        h->buckets[idx] = forward_list_construct();
+        forward_list_push_back(h->buckets[idx], (data_type*) item);
+
+    }
+    h->n_elements++;
+
+    return NULL;
+}
+
+void *hash_table_get(HashTable *h, void *key)
+{
+    int idx = h->hash_fn(h, key);
+
+    if(h->buckets[idx]){
+        Node *n = h->buckets[idx]->head;
+
+        if(n){
+            HashTableItem *item = (HashTableItem *) n->value;
+            return item->val;
+        }
+    }
+
+    return NULL;
+}
+
+void *hash_table_pop(HashTable *h, void *key)
+{
+    int idx = h->hash_fn(h, key);
+    Node *prev = NULL, *next;
+
+    if(h->buckets[idx]){
+        Node *n = h->buckets[idx]->head;
+
+        while(n){
+            next = n->next;
+
+            if(!h->cmp_fn(key, n)){
+                
+                prev->next = next;
+                n->next = NULL;
+                return n;
+            }            
+
+            prev = n;
+            n = next;
+        }
+    }
+
+    return NULL;
+}
+
+int hash_table_size(HashTable *h)
+{
+    return h->table_size;
+}
+
+int hash_table_num_elems(HashTable *h)
+{
+    return h->n_elements;
+}
+
+void hash_table_destroy(HashTable *h)
+{
+    for (int i = 0; i < h->table_size; i++)
+    {
+        if (h->buckets[i] != NULL)
+        {
+            Node *n = h->buckets[i]->head;
+
+            while (n != NULL)
+            {
+                HashTableItem *pair = n->value;
+                _hash_pair_destroy(pair);
+                n = n->next;
+            }
+
+            forward_list_destroy(h->buckets[i]);
+        }
+    }
+
+    free(h->buckets);
+    free(h);
+}
---
+++ hash.h
@@ -0,0 +1,40 @@
+
+#ifndef _HASH_TABLE_H_
+#define _HASH_TABLE_H_
+
+typedef struct HashTable HashTable;
+
+typedef int (*HashFunction)(HashTable *, void *);
+typedef int (*CmpFunction)(void *v1, void *v2);
+
+typedef struct
+{
+    void *key;
+    void *val;
+} HashTableItem;
+
+// criacao da tabela hash
+HashTable *hash_table_construct(int size, HashFunction hash_fn, CmpFunction cmp_fn);
+
+// funcao para insercao/atualizacao de pares chave-valor em O(1).
+// Eh responsabilidade do usuario gerar a chave como um inteiro.
+// internamente a tabela hash realiza o hash modular para colocar o elemento na posicao correta
+// lista encadeada sao usadas para tratar colisoes
+void *hash_table_set(HashTable *h, void* key, void *val);
+
+// retorna o valor associado com a chave key ou NULL se ela nao existir
+void *hash_table_get(HashTable *h, void* key);
+
+// remove o par chave-valor e retorna o valor ou NULL se nao existir tal chave
+void *hash_table_pop(HashTable *h, void* key);
+
+// numero de elementos
+int hash_table_num_elems(HashTable *h);
+
+// tamanho da tabela
+int hash_table_size(HashTable *h);
+
+// libera o espaco alocado para a tabela hash
+void hash_table_destroy(HashTable *h);
+
+#endif
